fbe146d7fc7651921c11397d9dbd1187de637fec
Owais.Sayed <Owais.Sayed@PC-33>
2023-07-03 14:24:40 +0530

    Add project files.

---
+++ W5VisibilityAttachedProp/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace W5VisibilityAttachedProp
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public bool ToggleVisibility { get; set; } = false;
+        public ICommand ToggleCommand { get; set; }
+        public MainWindowViewModel()
+        {
+            ToggleCommand = new RelayCommand(ChangeVisibility);
+        }
+        private void ChangeVisibility()
+        {
+            ToggleVisibility = !ToggleVisibility;
+            OnPropertyChanged(nameof(ToggleVisibility));
+        }
+    }
+}
---
+++ W5VisibilityAttachedProp/VisibilityProp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace W5VisibilityAttachedProp
+{
+    public class VisibilityProp
+    {
+        public static bool GetToggleVisibility(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ToggleVisibilityProperty);
+        }
+
+        public static void SetToggleVisibility(DependencyObject obj, int value)
+        {
+            obj.SetValue(ToggleVisibilityProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ToggleVisibilityProperty =
+            DependencyProperty.RegisterAttached("ToggleVisibility", typeof(bool), typeof(VisibilityProp), new PropertyMetadata(true, (a,b) =>
+            {
+                var CurrentValue = a.GetValue(ToggleVisibilityProperty);
+
+                UIElement t = (UIElement)a;
+                if (CurrentValue != null && CurrentValue is true)
+                    t.Visibility = Visibility.Visible;
+                else if(CurrentValue != null && CurrentValue is false)
+                    t.Visibility = Visibility.Collapsed;
+            }));
+
+
+    }
+}
